21e390d9b0f002670265b62cf202ae049f29fd93
asmaaselim <asmaaselim@icloud.com>
2017-12-24 18:52:26 +0200

    create models: order, address, auction &  admin

---
+++ app/models/auction.rb
@@ -0,0 +1,7 @@
+class Auction < ApplicationRecord
+	has_many :bids
+	# , dependent: :destroy
+	belongs_to :order
+
+	validates :bid_deadline, :status, presence: true 
+end
---
+++ db/migrate/20171224124358_create_addresses.rb
@@ -0,0 +1,15 @@
+class CreateAddresses < ActiveRecord::Migration[5.1]
+  def change
+    create_table :addresses do |t|
+    	t.integer :apartment_number
+    	t.integer :building_number
+      t.string :street
+      t.string :area
+      t.boolean :saved, default: false
+      t.references :client, foreign_key: true
+
+
+      t.timestamps
+    end
+  end
+end
