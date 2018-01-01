3b1e615f9145789fb64f3ef7758002b7e071aecc
asmaaselim <asmaaselim@icloud.com>
2018-01-01 21:00:04 +0200

    second merge

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171228100447) do
-# ActiveRecord::Schema.define(version: 20171226100144) do
 
+ActiveRecord::Schema.define(version: 20171226100144) do
+ActiveRecord::Schema.define(version: 20171228100447) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
